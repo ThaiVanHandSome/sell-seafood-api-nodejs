4a41771e746eb1668b0292ddb8778620df6c1a1f
ThaiVanHandSome <danitbadao1234@gmail.com>
2023-10-22 15:34:30 +0700

    add findAll Category

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 3000;
 const { conn, sql } = require("./DAO");
@@ -6,10 +7,21 @@ const bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
 
+app.use(cors({ origin: "http://localhost:3004" }));
+
 // http://localhost:3000/api/seafoods
 app.get("/api/seafoods", async (req, resp) => {
   var pool = await conn;
   var sqlQuery = "SELECT * FROM Seafood";
+  return await pool.request().query(sqlQuery, function (err, res) {
+    resp.send({ data: res.recordset });
+  });
+});
+
+// http://localhost:3000/api/categories
+app.get("/api/categories", async (req, resp) => {
+  var pool = await conn;
+  var sqlQuery = "SELECT * FROM Category";
   return await pool.request().query(sqlQuery, function (err, res) {
     resp.send({ data: res.recordset });
   });
